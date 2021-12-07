08dd2054b7b2fa802b6118e149d11be9296a8f27
ctejaxun <cesar.etx@gmail.com>
2021-12-07 01:41:27 -0600

    agrega placeholder para las todas las acciones del menu restante

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pyodbc
-import os
+import os, pyodbc, csv
 
 CONN_STR = "DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};"
 
@@ -12,10 +11,31 @@ DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
 class EtlHandler:
     def get_conn_str(self):
-        return CONN_STR.format(server="db", database="tempdb", username="sa", password=DB_PASSWORD, )
+        return CONN_STR.format(
+            server="db",
+            database="tempdb",
+            username="sa",
+            password=DB_PASSWORD,
+        )
 
     def create_connection(self):
         return pyodbc.connect(self.get_conn_str())
+
+    def create_information(self, pathfile):
+        with open(pathfile, mode="r") as file:
+            for item in csv.reader(file):
+                print(item)
+                break
+
+    def create_models(
+        self,
+    ):
+        print("done")
+
+    def make_querys(
+        self,
+    ):
+        print("done")
 
 
 handler = EtlHandler()
@@ -25,7 +45,8 @@ print(handler.create_connection())
 
 running = True
 while running:
-    print("""
+    print(
+        """
         MENU
         -----------------------------
         1. Crear modelo
@@ -33,9 +54,17 @@ while running:
         3. Realizar consultas
         4. Salir
         -----------------------------
-    """)
+    """
+    )
     option = input("Selecciona una opción:")
     if option == "4":
         running = False
+    elif option == "1":
+        handler.create_models()
+    elif option == "2":
+        pathfile = input("Ingrese ruta absoluta del archivo:")
+        handler.create_information(pathfile or "/workspace/tsunamies.csv")
+    elif option == "3":
+        handler.make_querys()
     else:
         print("OPción no reconocida")
